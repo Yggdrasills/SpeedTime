45f8f715d1a42ea703622b8bc9f6f00e86ae4539
Yggdrasil <angry_smile@inbox.ru>
2020-06-18 00:18:59 +0500

    Added base scripts

---
+++ Assets/Scripts/Utils/Editor/NamespaceModificationProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace BaseCore.Utils.Editor
+{
+    public class NamespaceModificationProcessor : UnityEditor.AssetModificationProcessor
+    {
+        public static void OnWillCreateAsset(string path)
+        {
+            path = path.Replace(".meta", string.Empty);
+
+            if (!path.Contains(".cs")) return;
+
+            int index = Application.dataPath.LastIndexOf("Assets");
+            path = Application.dataPath.Substring(0, index) + path;
+
+            if (!File.Exists(path)) return;
+
+            string fileContent = File.ReadAllText(path);
+
+            fileContent = fileContent.Replace("#NAMESPACE#", BuildNamespaceByPath(path));
+
+            File.WriteAllText(path, fileContent);
+            AssetDatabase.Refresh();
+        }
+
+        private static string BuildNamespaceByPath(string path)
+        {
+            string[] pathFolders = path.Replace(Application.dataPath, string.Empty).Split('/');
+            string nameSpace = "SpeedTime.";
+
+            for (int i = 2; i < pathFolders.Length - 1; i++)
+            {
+                pathFolders[i] = pathFolders[i].Replace(' ', '_');
+                nameSpace += pathFolders[i] + ".";
+            }
+
+            return nameSpace.Substring(0, nameSpace.Length - 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Editor/SpriteRendererPreview.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BaseCore.Utils.Editor
+{
+    [CustomPreview(typeof(SpriteRenderer))]
+    public class SpriteRendererPreview : ObjectPreview
+    {
+        private Texture _sprite = null;
+
+        private bool HasSprite()
+        {
+            var spriteRenderer = (target as SpriteRenderer).GetComponent<SpriteRenderer>();
+
+            if (spriteRenderer.sprite != null)
+            {
+                _sprite = AssetPreview.GetAssetPreview(spriteRenderer.sprite);
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool HasPreviewGUI()
+        {
+            return HasSprite();
+        }
+
+        public override void OnPreviewGUI(Rect r, GUIStyle background)
+        {
+            EditorGUI.DrawTextureTransparent(r, _sprite, ScaleMode.ScaleToFit);
+        }
+    }
+
+}
